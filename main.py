d3449df73b0539386c01c30e0f2f6e95822f0417
Varun Mishra <varun21999@gmail.com>
2025-10-30 16:51:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ from enums import MediaType
 
 if __name__ == "__main__":
     # Create users
-    alice = User("U1", "Alice", "+911234567890")
-    bob = User("U2", "Bob", "+919876543210")
+    varun = User("001", "Varun", "+917474747474")
+    anuj = User("002", "Anuj", "+919876543210")
 
     # 1:1 Chat Example
-    private_chat = PrivateChat("C1", alice, bob)
-    msg1 = TextMessage("M1", alice, "Hey Bob! How are you?")
-    alice.send_message(private_chat, msg1)
+    private_chat = PrivateChat("C1", varun, anuj)
+    msg1 = TextMessage("M1", varun, "Hey Anuj! How are you?")
+    varun.send_message(private_chat, msg1)
 
     # Group Chat Example
-    group_chat = GroupChat("G1", "Weekend Plans", alice)
-    group_chat.add_participant(bob)
-    msg2 = MediaMessage("M2", alice, "pic.jpg", MediaType.IMAGE)
-    alice.send_message(group_chat, msg2)
+    group_chat = GroupChat("G1", "Weekend Plans", varun)
+    group_chat.add_participant(anuj)
+    msg2 = MediaMessage("M2", varun, "pic.jpg", MediaType.IMAGE)
+    varun.send_message(group_chat, msg2)
 
     print("\nChat History (Group):", group_chat.get_chat_history())
